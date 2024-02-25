1d8d696fd3408188aa446de0249302d3eeaef745
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-02-25 15:52:16 -0300

    c

--- RUST_Exercicios/exercicio02_Rust/target/debug/deps/my_project-bf8cbe29ab0f79ec.d
+++
@@ -1,5 +0,0 @@
-/home/runner/Exercicio02Rust/target/debug/deps/my_project-bf8cbe29ab0f79ec.rmeta: src/main.rs
-
-/home/runner/Exercicio02Rust/target/debug/deps/my_project-bf8cbe29ab0f79ec.d: src/main.rs
-
-src/main.rs:
